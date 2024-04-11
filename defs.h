090734eb3794c17b3994dfc67af30dde1a4b4eb3
victorivis <knightdragonred@gmail.com>
2024-04-11 20:29:29 -0300

    Definições iniciais

---
+++ defs.h
@@ -0,0 +1,65 @@
+#ifndef DEFS_H
+#define DFS_H
+
+typedef unsigned long long U64;
+
+#define NAME "Vice 1.0"
+#define BRD_SQ_NUM 120
+
+#define MAXGAMEMOVES 2048
+
+enum { EMPTY, wP, wN, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
+enum { WHITE, BLACK, BOTH };
+
+enum {
+    A1 = 21, B1, C1, D1, E1, F1, G1, H1,
+    A2 = 31, B2, C2, D2, E2, F2, G2, H2,
+    A3 = 41, B3, C3, D3, E3, F3, G3, H3,
+    A4 = 51, B4, C4, D4, E4, F4, G4, H4,
+    A5 = 61, B5, C5, D5, E5, F5, G5, H5,
+    A6 = 71, B6, C6, D6, E6, F6, G6, H6,
+    A7 = 81, B7, C7, D7, E7, F7, G7, H7,
+    A8 = 91, B8, C8, D8, E8, F8, G8, H8, NO_SQ
+};
+
+enum { FALSE, TRUE };
+
+enum { WKCA = 1, WQCA = 2, BKCA = 4, BQCA = 8 };
+
+typedef struct{
+    int move;
+    int castlePerm;
+    int enPas;
+    int fiftyMove;
+    U64 posKey;
+
+} S_UNDO;
+
+typedef struct{
+    int pieces[BRD_SQ_NUM];
+    U64 pawns[3];
+
+    int KingSq[2];
+
+    int side;
+    int enPas;
+    int fiftyMove;
+
+    int ply;
+    int hisPly;
+
+    int castlePerm;
+
+    U64 posKey;
+    
+    int pceNum[13];
+    int bigPce[3];
+    int majPce[3];
+    int minPce[3];
+
+    S_UNDO history[MAXGAMEMOVES];
+
+} S_BOARD;
+
+#endif
